b605d3712623b0d623dfb0e4dfb2c6d244d8b0a4
Irfan <irfan.abdulhameed85@gmail.com>
2024-07-16 23:59:02 +0530

    Added Logo Successfully, tried creating .env file for the tokens but failed.

--- lib/main.dart
+++ lib/main.dart
@@ -246,11 +246,13 @@
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 import 'package:lottie/lottie.dart';
 
 void main() {
+  // await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
